12a9b93452aaf9221b649cab43abdd415e3e8513
Jacob <jross9111@conestogac.on.ca>
2025-02-28 00:00:10 -0500

    sprite animations. jump velocity fixes stun on hit windows sound

--- player/player.gd
+++ player/player.gd
@@ -3,46 +3,58 @@ extends CharacterBody2D
 
 const WALK_SPEED = 300.0
 const ACCELERATION_SPEED = WALK_SPEED * 6.0
-const JUMP_VELOCITY = -950.0
-const MAX_CHARGE_TIME = 1.0
+const JUMP_VELOCITY = -900.0
+const MAX_CHARGE_TIME = 1.2
 const MAX_POWER = 100
-const MIN_JUMP_VELOCITY = -750.0
+const MIN_JUMP_VELOCITY = -600.0
 const TERMINAL_VELOCITY = 700
+const STUN_DURATION := 0.5
+
 
 @export var action_suffix := ""
 
 var gravity: int = ProjectSettings.get("physics/2d/default_gravity")
 @onready var platform_detector := $PlatformDetector as RayCast2D
-@onready var animation_player := $AnimationPlayer as AnimationPlayer
-@onready var sprite := $Sprite2D as Sprite2D
 @onready var jump_sound := $Jump as AudioStreamPlayer2D
 @onready var camera := $Camera as Camera2D
 @onready var charge_bar := $UI/ChargeBar as TextureProgressBar
 @onready var text_particles := $TextParticles as CPUParticles2D
 @onready var particles_container := $ParticlesContainer as Control
 @onready var current_i_label := $UI/CurrentILabel as Label
-
+@onready var animated_sprite := $AnimatedDuck as AnimatedSprite2D
 
 var _is_charging_jump := false
 var _charge_time := 0.0
 var code_texts = ["printf()", "console.log()", "print()", "System.out.println()", "cout<<", "Debug.Log()", "echo", "fmt.Println()"]
+# Add stun timer for knockback/bounce effects
+var _stunned := false
+var _stun_timer := 0.0
 
 func _ready() -> void:
 	text_particles.emitting = false
 	update_power_loop_label(0)
 
 func apply_bounce(bounce_force: float):
-	print('bounce')
 	velocity.y = bounce_force  # Apply upward force
+	_stunned = true
+	_stun_timer = 0.0
 	move_and_slide()
 
 func apply_knockback(force: Vector2):
 	print('collide')
 	velocity = force
+	_stunned = true
+	_stun_timer = 0.0
 	move_and_slide()
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("jump" + action_suffix):
+	# Update stun timer if stunned
+	if _stunned:
+		_stun_timer += delta
+		if _stun_timer >= STUN_DURATION:
+			_stunned = false
+
+	if Input.is_action_just_pressed("jump" + action_suffix) and not _stunned:
 		_start_charging_jump()
 	elif Input.is_action_just_released("jump" + action_suffix):
 		_stop_charging_jump()
@@ -54,24 +66,29 @@ func _physics_process(delta: float) -> void:
 	# Fall.
 	velocity.y = minf(TERMINAL_VELOCITY, velocity.y + gravity * delta)
 
-	var direction := Input.get_axis("move_left" + action_suffix, "move_right" + action_suffix) * WALK_SPEED
-	velocity.x = move_toward(velocity.x, direction, ACCELERATION_SPEED * delta)
+	# Only allow horizontal movement when not stunned
+	if not _stunned:
+		var direction := Input.get_axis("move_left" + action_suffix, "move_right" + action_suffix) * WALK_SPEED
+		velocity.x = move_toward(velocity.x, direction, ACCELERATION_SPEED * delta)
+	else:
+		# Optional: Add a small amount of air resistance while stunned
+		velocity.x = move_toward(velocity.x, 0, ACCELERATION_SPEED * 0.05 * delta)
 
 	if not is_zero_approx(velocity.x):
 		if velocity.x > 0.0:
-			sprite.scale.x = 1.0
+			animated_sprite.scale.x = 2.0
 		else:
-			sprite.scale.x = -1.0
+			animated_sprite.scale.x = -2.0
 
 	floor_stop_on_slope = not platform_detector.is_colliding()
 	move_and_slide()
 
 	var animation := get_new_animation()
-	if animation != animation_player.current_animation:
-		animation_player.play(animation)
+	if animation != animated_sprite.get_animation():
+		animated_sprite.play(animation)
 
-func get_new_animation() -> String:
-	var animation_new: String
+func get_new_animation() -> StringName:
+	var animation_new: StringName
 	if is_on_floor():
 		if absf(velocity.x) > 0.1:
 			animation_new = "run"
@@ -100,14 +117,11 @@ func try_jump() -> void:
 	if is_on_floor():
 		jump_sound.pitch_scale = 1.0
 	else:
-		 # Only allow jumping when on the floor
 		return 
 
 	# Calculate jump velocity based on charge time
 	var charge_ratio: float = min(_charge_time / MAX_CHARGE_TIME, 1.0)
-	var jump_velocity: float = JUMP_VELOCITY * charge_ratio
-	jump_velocity = max(jump_velocity, MIN_JUMP_VELOCITY)
-
+	var jump_velocity: float = MIN_JUMP_VELOCITY + (JUMP_VELOCITY - MIN_JUMP_VELOCITY) * charge_ratio
 	velocity.y = jump_velocity
 	jump_sound.play()
 	
@@ -130,7 +144,7 @@ func emit_text_particles(charge_ratio: float) -> void:
 	particles_container.add_child(label_node)
 	
 	# Set starting position (at player's feet)
-	var random_x_offset = randf_range(-30, 30)
+	var random_x_offset = randf_range(-10, 10)
 	label_node.position = Vector2(random_x_offset, 10)  # Start below player's feet
 	
 	# Set up a timer to remove the label after a short time
